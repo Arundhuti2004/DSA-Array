97f7ecba6e26e98dee3309abe08dcfb746f3574e
ARUNDHUTI SARKAR <121664607+Arundhuti2004@users.noreply.github.com>
2023-08-13 07:33:04 +0530

    Update largest_element.cpp

--- largest_element.cpp
+++ largest_element.cpp
@@ -1,19 +1,21 @@
 //largest element in the array
-#include<iostream>
-#include<limits.h>
+#include <iostream>
+#include <limits.h>
 using namespace std;
-int main(){
-int max = INT_MIN;
-int n;
-cin >> n;
-int arr[n];
-for(int i = 0 ; i < n ; i++)
-    cin >> arr[i];
-for(int i  = 0 ; i < n ; i++){
-    if(arr[i] > max)
-        max = arr[i];
+int main()
+{
+    int max = INT_MIN;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
     }
     cout << max;
 
-return 0 ;
+    return 0;
 }
