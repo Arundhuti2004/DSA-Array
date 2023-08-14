0e4ba3244c699b60911405ac083227ce91536618
ARUNDHUTI SARKAR <121664607+Arundhuti2004@users.noreply.github.com>
2023-08-14 21:22:37 +0530

    Add files via upload

---
+++ second_largest.cpp
@@ -0,0 +1,31 @@
+//second largest element in array
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main()
+{
+    int max = INT_MIN;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max){
+            max = arr[i];
+        }
+        
+
+
+
+    }
+    cout << max;
+    for(int i = 0 ; i < n ;i++){
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    cout << max;
+
+    return 0;
+}
